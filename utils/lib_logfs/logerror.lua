7b824c8575c3fc147fa8f7fd1fbced5eba5bcc83
あっきー ( akki ) <151368291+akki697222@users.noreply.github.com>
2024-03-18 20:15:14 +0900

    怪しい英(ry

--- utils/lib_logfs/logerror.lua
+++ utils/lib_logfs/logerror.lua
@@ -1,7 +1,7 @@
 local le = {}
 
 function le.error(errortext)
-    error("Encountered an except: exception.".. errortext .." Computer was restarting on 5 seconds...", 0)
+    error("Encountered an except: exception.".. errortext .." Computer was restart on 5 seconds...", 0)
     sleep(5)
     shell.run("reboot")
 end
