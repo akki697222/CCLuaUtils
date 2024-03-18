b64f6bd6526e016e3a2200f4cc9de6191b55b607
akki697222 <newakki69@outlook.jp>
2024-03-18 21:46:29 +0900

    インストール用スクリプトを追加

--- lib_download_all.lua
+++ lib_download_all.lua
@@ -6,10 +6,17 @@ function mkdir(dir)
     shell.run("mkdir "..dir)
 end
 
+function wget(url, path)
+    cd(path)
+    shell.run("wget "..url)
+end
+
 function main()
     mkdir("libs")
     cd("libs")
     mkdir("lib_libloader")
     mkdir("lib_logfs")
     mkdir("lib_utils")
+    wget("https://raw.githubusercontent.com/akki697222/CCLuaUtils/main/libs/lib_logfs/log_critical.lua", "/libs/lib_logfs")
+    wget("https://raw.githubusercontent.com/akki697222/CCLuaUtils/main/libs/lib_libloader/libloader.lua", "/libs/lib_libloader")
 end
